b3691a479b34036440c1ce8f7db85f18c640c0bc
La svedese <5554930+sarasjodin@users.noreply.github.com>
2023-12-15 15:36:27 +0100

    Deafult value added for the expectations box

--- src/script.js
+++ src/script.js
@@ -41,12 +41,13 @@ function generatePoem(event) {
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.defaultValue =
+  "Please help me create a poem for my dearest friend, who always supports me and always have time to listen. We first got to know each other at work and then we started seeing each other also on our spare time. When I want to celebrate something or share a burden my friend is always there for me.";
+console.log(poemFormElement.defaultValue); // Prints the default value
 poemFormElement.addEventListener("submit", generatePoem);
 
 // Get the input element
 let input = document.getElementById("poem-topic-box");
-
-// ... (your existing code for typewriter and poem generation)
 
 // Create "Create another poem" button dynamically
 let createAnotherPoemButton = document.createElement("button");
@@ -66,58 +67,33 @@ document.getElementById("submit-button").addEventListener("click", function () {
   createAnotherPoemButton.style.display = "inline-block";
 });
 
-// Define a function to adjust the input width
-/* function adjustWidth() {
- */ // Get the input value
-/*   let value = input.value;
- */
-// Get the number of characters in the input value
-/*   let length = value.length;
- */
-// Set the input width to 10 pixels times the number of characters
-/*   input.style.width = 5 * length + "px";
-}
- */
-// Create an array of poem types
-let poemTypes = [
-  "Random",
-  "Sonnet",
-  "Villanelle",
-  "Haiku",
-  "Ballad",
-  "Limerick",
-  "Free verse",
-  "Acrostic",
-  "Cinquain",
-  "Epic",
-];
+let textInfo = document.createElement("p");
+textInfo.id = "text-info";
+textInfo.style.display = "none"; // Hide the text information initially
 
-// Generate a random index from the array
-let randomIndex = Math.floor(Math.random() * poemTypes.length);
+// Append the text information to the main container
+document.querySelector("main").appendChild(textInfo);
 
-// Access the corresponding element from the array
-let randomPoemType = poemTypes[randomIndex];
+// Add an event listener to the form submit event
+poemFormElement.addEventListener("submit", function (event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
 
-// Store the URL of the API endpoint
-let url = "https://www.poem-generator.org.uk/api/";
+  // Get the value of the textarea
+  let value = input.value;
 
-// Store the parameters of the API request
-let params = { type: randomPoemType, keywords: "some keywords" };
-
-/* Define an async function
-async function makePostRequest() {
-  try {
-    // Make a POST request with axios and await the response
-    let response = await axios.post(url, params);
-
-    // Handle the response
-    console.log(response.data);
-  } catch (error) {
-    // Handle the error
-    console.error(error);
+  // Check if the value is equal to the default value
+  if (value == poemFormElement.defaultValue) {
+    // Set the text information
+    textInfo.textContent =
+      "*In the box where you are supposed to add your poem expectations, no additional information was added, therefore this poem was generated with the following default text: " +
+      poemFormElement.defaultValue;
+    // Show the text information
+    textInfo.style.display = "block";
+  } else {
+    // Clear the text information
+    textInfo.textContent = "";
+    // Hide the text information
+    textInfo.style.display = "none";
   }
-}
-
-// Call the async function
-makePostRequest();
-*/
+});
