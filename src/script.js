d03120a53d0eaac94aac321dac5101a8f7b9efdb
La svedese <5554930+sarasjodin@users.noreply.github.com>
2023-12-19 12:56:04 +0100

    Started to collect form data

--- src/script.js
+++ src/script.js
@@ -1,40 +1,131 @@
 // Type writing functionality
 let heading = document.querySelector("#typewriter");
 let text = "generator";
-let speed = 120;
-let delay = 3000;
-let pause = 500;
-let index = 0;
+let speed1 = 120;
+let delay1 = 3000;
+let pause1 = 500;
+let index1 = 0;
+let isTypeNextWordActive = false; // Variable to track if typeNextWord is active
+
 function typeNextChar() {
+  // If typeNextWord is active, pause typeNextChar
+  if (isTypeNextWordActive) {
+    setTimeout(typeNextChar, 100); // Adjust the pause duration as needed
+    return;
+  }
+
   // If the index is less than the text length, type the next character
-  if (index < text.length) {
+  if (index1 < text.length) {
     // Append the next character to the h1 element
-    heading.innerHTML += text.charAt(index);
+    heading.innerHTML += text.charAt(index1);
     // Increment the index
-    index++;
+    index1++;
     // Set a timer to call this function again after the speed delay
-    setTimeout(typeNextChar, speed);
+    setTimeout(typeNextChar, speed1);
   } else {
     // Set a timer to clear the h1 element after the delay
     setTimeout(function () {
       heading.innerHTML = "";
       // Set another timer to call this function again after the pause
-      setTimeout(typeNextChar, pause);
-    }, delay);
+      setTimeout(typeNextChar, pause1);
+    }, delay1);
     // Reset the index
-    index = 0;
+    index1 = 0;
   }
 }
 
 typeNextChar();
 
+// Type writing functionality for words
+let words = []; // You need to set this array with words from your response
+let typewritingPoem = document.querySelector("#poem-container");
+let speed2 = 120;
+let delay2 = 3000;
+let pause2 = 500;
+let index2 = 0;
+
+function typeNextWord() {
+  // If the index is less than the number of words
+  if (index2 < words.length) {
+    // Append the next word to the h1 element
+    typewritingPoem.innerHTML += words[index2] + " ";
+    // Increment the index
+    index2++;
+    // Set a timer to call this function again after the speed delay
+    setTimeout(typeNextWord, speed2);
+  } else {
+    // Reset the index
+    index2 = 0;
+    isTypeNextWordActive = false; // Set typeNextWord as inactive
+  }
+}
+
+function collectFormData() {
+  // Select the form element
+  let form = document.getElementById("poem-form-element");
+
+  // Create an array to store the collected data
+  let formData = [];
+
+  // Iterate through form elements
+  for (let i = 0; i < form.elements.length; i++) {
+    let element = form.elements[i];
+
+    // Check element type
+    switch (element.type) {
+      case "radio":
+      case "checkbox":
+        // Check if the radio button or checkbox is checked
+        if (element.checked) {
+          // Add the custom-data attribute to the formData array
+          formData.push(element.getAttribute("custom-data"));
+        }
+        break;
+      case "textarea":
+      case "text":
+        // Check if the textarea or text input has a value
+        if (element.value.trim() !== "") {
+          // Add the text value to the formData array
+          formData.push(element.value);
+        }
+        break;
+      case "select-one":
+        // Check if the select element has a selected option
+        if (element.selectedIndex !== -1) {
+          // Add the custom-data attribute to the formData array
+          formData.push(
+            element.options[element.selectedIndex].getAttribute("custom-data")
+          );
+        }
+        break;
+    }
+  }
+
+  // Convert the collected data to a single string
+  let jsonData = JSON.stringify([formData.join(" ")], null, 2).replace(
+    /\"/g,
+    "'"
+  );
+
+  // Add logs to trace the flow
+  console.log("Collected Form Data:", formData);
+  console.log("JSON Data:", jsonData);
+
+  // You can now use jsonData as needed (e.g., send it to the server)
+  console.log(jsonData);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   let poemContainer = document.querySelector("#poem-container");
   let submitButton = document.querySelector("#submit-button");
-  let reloadButton; // Declare reloadButton in the outer scope
+  let introductionParagraph = document.getElementById("introduction");
+  let reloadButton;
+  let defaultWaitingMessage;
 
   submitButton.addEventListener("click", async function (event) {
     event.preventDefault(); // Prevent the default form submission behavior    //
+    console.log("Submit button clicked");
+    collectFormData(); // Add this line
     // Hide the form if it exists
     let poemFormElement = document.querySelector("#poem-form-element");
     if (poemFormElement) {
@@ -43,6 +134,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Show the poem container
     poemContainer.style.display = "block";
+
+    // Update introduction text when poemContainer becomes visible
+    if (introductionParagraph) {
+      introductionParagraph.textContent = "I'm Sara, your";
+    }
 
     async function makeGetRequest(apiUrl, apiKey) {
       let headers = {
@@ -53,6 +149,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const response = await axios.get(apiUrl);
         return response.data.answer;
       } catch (error) {
+        console.error("Error making GET request:", error);
         throw error;
       }
     }
@@ -60,20 +157,42 @@ document.addEventListener("DOMContentLoaded", function () {
     async function fetchData(apiUrl, apiKey) {
       try {
         const response = await makeGetRequest(apiUrl, apiKey);
-        // Display generated poem
-        poemContainer.textContent = response;
-      } catch (error) {}
+
+        // Update introduction text when poemContainer becomes visible
+        if (introductionParagraph) {
+          introductionParagraph.textContent = "Enjoy my poem!";
+          defaultWaitingMessage.style.display = "none";
+          // Display generated poem
+          /* poemContainer.textContent = response; */
+          // Display generated poem
+          words = response.split(" "); // Split the poem into words
+          console.log(words);
+          typeNextWord(); // Start typing words
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        alert("Check your internet connection.");
+      }
     }
 
     // Example usage:
-    let prompt = "love";
-    let context = "poem";
+    let prompt = "A short 2 row rhyme couplets about love";
+    let context = " and poem";
     let apiKey = "7166fo94tf124f43f80bab7387ed0a26";
 
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     // Invoke the fetchData function
     fetchData(apiUrl, apiKey);
+
+    // Create defaultWaitingMessage dynamically
+    defaultWaitingMessage = document.createElement("div");
+    defaultWaitingMessage.id = "waiting-message";
+    defaultWaitingMessage.innerText = "Please wait for your poem...";
+    defaultWaitingMessage.style.display = "none";
+
+    // Append the defaultWaitingMessage before the reloadButton
+    poemContainer.parentElement.appendChild(defaultWaitingMessage);
 
     reloadButton = document.createElement("button");
     reloadButton.id = "reload-button";
@@ -85,8 +204,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelector("main").appendChild(reloadButton);
 
+    // Append the reloadButton to the same parent as poemContainer
+    poemContainer.parentElement.appendChild(reloadButton);
+
     // Ensure reloadButton is defined before modifying its style
     if (reloadButton) {
       reloadButton.style.display = "inline-block";
-  };
-});})
+    }
+
+    // Use requestAnimationFrame to ensure proper timing for visibility toggle
+    requestAnimationFrame(function () {
+      defaultWaitingMessage.style.display = "block";
+    });
+  });
+});
